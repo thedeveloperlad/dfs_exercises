dcf937ba0c1738df835ca1c090a8d8c714d0f1ea
leonardo <lopez.leonardo@uabc.edu.mx>
2026-07-01 08:41:14 -0700

    [Basic Binary Tree]: Added Binary Search Tree and Queue Insertion approach.

--- BinaryTree.June.22.2026/CreatingBinaryTree_22_June_2026.cpp
+++ BinaryTree.June.22.2026/CreatingBinaryTree_22_June_2026.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 struct Node {
@@ -23,6 +24,78 @@ int getSize(Node* root) {
 	}
 }
 
+// Recursive insertion for a Binary Search Tree.
+void insertBST(Node* root, int data)
+{
+	if (root == nullptr)
+	{
+		root = new Node(data);
+		return;
+	}
+	else
+	{
+		if (data < root->data)
+		{
+			insertBST(root->left, data);
+		}
+		else {
+			insertBST(root->right, data);
+		}
+	}
+}
+
+// Level-order insertion using a tracking queue
+Node* insertNode(Node* root, int data)
+{
+	if (root == nullptr) {
+		Node* root = new Node(data);
+		return root;
+	}
+	else
+	{
+		queue<Node*> q;
+		q.push(root);
+
+		Node* curr = q.front();
+		q.pop();
+
+		while (!q.empty())
+		{
+
+			if (curr->left == nullptr)
+			{
+				q.push(curr->left);
+			}
+			else {
+				curr->left = new Node(data);
+				return root;
+			}
+
+			if (curr->right == nullptr)
+			{
+				q.push(curr->right);
+			}
+			else {
+				curr->right = new Node(data);
+				return root;
+			}
+		}
+	}
+}
+
+void printInOrder(Node* root)
+{
+	if (root == nullptr)
+	{
+		return;
+	}
+	else {
+		printInOrder(root->left);
+		cout << "Tree Size: " << root->data << '\n';
+		printInOrder(root->right);
+	}
+}
+
 /*
 
 		2
@@ -30,8 +103,9 @@ int getSize(Node* root) {
 5		6
 */
 
-int main() {
-    // Initilize and allocate memory for tree nodes
+void testRoot()
+{
+	// Initilize and allocate memory for tree nodes
 	Node* firstNode = new Node(2);
 	Node* secondNode = new Node(3);
 	Node* thirdNode = new Node(4);
@@ -44,7 +118,23 @@ int main() {
 	secondNode->left = fourthNode;
 	thirdNode->left = fiveNode;
 
-	
+
 	cout << "Tree Size: " << getSize(firstNode) << '\n';
+}
+
+int main() {
+    // Initilize and allocate memory for tree nodes
+	Node* root = nullptr;
+
+	//Using the BST approach
+	insertBST(root, 2);
+	insertBST(root, 3);
+	insertBST(root, 4);
+	insertBST(root, 5);
+	insertBST(root, 6);
+
+	cout << "BST In-Order tranversal: ";
+	printInOrder(root);
+
     return 0;
 }
